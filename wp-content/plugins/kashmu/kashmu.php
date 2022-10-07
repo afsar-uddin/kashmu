3f46b11ae96ff4752cbac1bde1ff789256becf61
Afsar Uddin <afsarwebdev@gmail.com>
2022-10-08 00:17:16 +0600

    initial setup

---
+++ wp-content/plugins/kashmu/kashmu.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @package kashmu
+ */
+
+ /*
+  Plugin Name: Kashmu
+  Plugin URI: http://www.afsarbd.com/plugins
+  Description: This is first attempt on writing a custom plugin
+  Version: 1.0.0
+  Author: Afsar Uddin
+  Author URI: http://www.afsarbd.com
+  License: GPLv2 or later
+  Text Domain: kashmu
+  */
+
+  /*
+  This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundtion; either version 2 of the License, or (at your option) any later version.
+
+  This is distributed in the hope that it will be usefull, 
+  but WITHOUT ANY WARANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. 
+  
+  You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, INC., 51 Franklin street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+  Copyright 2005-2015 Automatic, Inc.
+  */
